2037eb7a9716752b99562b3ad67fe133c2c74f49
Oliver Atkins <oatkins@users.noreply.github.com>
2020-02-01 21:10:40 -0500

    New folder structure

--- publish.bat
+++ publish.bat
@@ -1,6 +1,7 @@
 pushd %1
+md publish
 del publish\*.* /q
-dotnet publish -o publish -c %2
+dotnet publish .\src\Pi.HifiControl\pi-hifi-control.csproj -o publish -c %2
 ssh pi@raspberrypi rm -fr pi-hifi-control/*
 scp publish\* pi@raspberrypi:pi-hifi-control
 popd
