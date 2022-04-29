122843bd77b1a7a5109a01ae7b4bf4f36ee796ac
Oliver Atkins <oatkins@users.noreply.github.com>
2022-04-29 09:29:18 -0400

    Publish no longer standalone

--- publish.bat
+++ publish.bat
@@ -1,7 +1,9 @@
 pushd %1
 md publish
 del publish\*.* /q
-dotnet publish .\src\Pi.HifiControl\pi-hifi-control.csproj --self-contained -r linux-arm -o publish -c %2 -p:PublishReadyToRunComposite=true -p:PublishReadyToRun=true
+# dotnet publish .\src\Pi.HifiControl\pi-hifi-control.csproj --self-contained -r linux-arm -o publish -c %2 -p:PublishReadyToRunComposite=true -p:PublishReadyToRun=true
+dotnet publish .\src\Pi.HifiControl\pi-hifi-control.csproj -o publish -c %2 -p:PublishSingleFile=false -p:PublishTrimmed=false
+ssh pi@raspberrypi mkdir pi-hifi-control
 ssh pi@raspberrypi rm -fr pi-hifi-control/*
 scp publish\* pi@raspberrypi:pi-hifi-control
 ssh pi@raspberrypi chmod 777 pi-hifi-control/pi-hifi-control
